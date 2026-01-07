8c08013c4e5a84370a629a38af577e8913b4f6bb
Aarohi <aarohi.verma@metasquaretech.com>
2026-01-07 14:32:15 +0530

    private folder making

---
+++ my-app/app/_lib/page.tsx
@@ -0,0 +1,12 @@
+function page() {
+  return (
+    <div>
+      <h1>
+        You cannot view this in the browser because of "_" or use "%5F" in URL
+        for private folders
+      </h1>
+    </div>
+  );
+}
+
+export default page;
