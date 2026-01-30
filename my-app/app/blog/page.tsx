e43e9e50d0a6a5d2884f8d23e572799aaa0a37e7
Aarohi-mst <aarohi.verma@metasquaretech.com>
2026-01-30 12:06:47 +0530

    title metadata

--- my-app/app/blog/page.tsx
+++ my-app/app/blog/page.tsx
@@ -1,5 +1,13 @@
-function page() {
+import { Metadata } from "next";
+
+export const metadata: Metadata= {
+  // title: "Blog",
+  title: {
+    absolute: "Blog",
+  }
+}
+
+export default function Blog() {
   return <div>Blog</div>;
 }
 
-export default page;
